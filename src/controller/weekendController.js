413ce1dea00d2df90e10092aae9aff6b2720536b
Ben Thomas <bthomas2622@gmail.com>
2018-07-30 21:14:13 -0400

    fixed logic issue for determining the number of weekend routes

--- src/controller/weekendController.js
+++ src/controller/weekendController.js
@@ -3,26 +3,42 @@ import AgencyKeyMapper from '../util/AgencyKeyMapper.json';
 
 const weekendController = async (req, res) => {
   const { agency } = req.query;
-  const MongoModel = modelHash['calendar.txt'].model;
-  MongoModel.find({ agency_key: AgencyKeyMapper[agency.toLowerCase()] }, (err, docs) => {
-    if (err) {
-      console.error(err);
+  const CalendarMongoModel = modelHash['calendar.txt'].model;
+  const TripsMongoModel = modelHash['trips.txt'].model;
+  const serviceIds = [];
+  CalendarMongoModel.find({ agency_key: AgencyKeyMapper[agency.toLowerCase()] }, (calendarErr, calendarDocs) => {
+    if (calendarErr) {
+      console.error(calendarErr);
       res.status(500).send('DB Error');
     } else {
-      let weekendRouteCount = 0;
-      docs.forEach((doc) => {
+      const routeIds = {};
+      calendarDocs.forEach((doc) => {
         const docData = doc._doc;
         if (Object.prototype.hasOwnProperty.call(docData, 'saturday')) {
           if (docData.saturday === 1) {
-            weekendRouteCount += 1;
+            serviceIds.push(docData.service_id);
           } else if (Object.prototype.hasOwnProperty.call(docData, 'sunday')) {
             if (docData.sunday === 1) {
-              weekendRouteCount += 1;
+              serviceIds.push(docData.service_id);
             }
           }
         }
       });
-      res.status(200).send({ NumWeekendRoutes: weekendRouteCount });
+      TripsMongoModel.find({ agency_key: AgencyKeyMapper[agency.toLowerCase()], service_id: { $in: serviceIds } }, (tripErr, tripDocs) => {
+        if (tripErr) {
+          console.error(tripErr);
+          res.status(500).send('DB Error');
+        } else {
+          tripDocs.forEach((doc) => {
+            const docData = doc._doc;
+            if (!Object.prototype.hasOwnProperty.call(routeIds, docData.route_id)) {
+              routeIds[docData.route_id] = null;
+            }
+          });
+        }
+        const numWeekendRoutes = Object.keys(routeIds).length;
+        res.status(200).send({ NumWeekendRoutes: numWeekendRoutes });
+      });
     }
   });
 };
