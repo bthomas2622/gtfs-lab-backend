9f30812db5a9bf4de8f9bb55dcb53d9137c80bdd
Ben Thomas <bthomas2622@gmail.com>
2019-03-06 21:05:17 -0800

    start integration tests

---
+++ integration-test/app.spec.js
@@ -0,0 +1,20 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { describe, it } from 'mocha';
+import app from '../src/app';
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('/GET app info', () => {
+  it('should GET package name', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.a('Object');
+        expect(res.body).to.deep.equal({ app: 'gtfs-lab-backend' });
+        done();
+      });
+  });
+});
